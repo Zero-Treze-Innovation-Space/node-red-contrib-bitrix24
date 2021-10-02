3a3863f31659856d732c77cfeff970cc11425634
Diego Lopes <diego-chez@hotmail.com>
2021-10-02 17:48:46 -0300

    feat: get vars from config node

--- nodes/bitrix-lead.js
+++ nodes/bitrix-lead.js
@@ -4,7 +4,13 @@ module.exports = function (RED) {
   function BitrixLead(n) {
     RED.nodes.createNode(this, n)
     
+    // Getting config vars
+    const authNode = RED.nodes.getNode(n.auth)
+    const portalName = authNode.portalName
+    const token = authNode.credentials.token
+    
     this.on("input", ((msg, send, done) => {
+      msg.payload = { portalName, token }
       send(msg)
       done()
     }))
--- nodes/bitrix-webhook-auth.js
+++ nodes/bitrix-webhook-auth.js
@@ -1,11 +1,12 @@
 module.exports = function (RED) {
   function BitrixWebhookAuth(config) {
     RED.nodes.createNode(this, config);
+    this.portalName = config.portalName
   }
 
   RED.nodes.registerType("bitrix-webhook-auth", BitrixWebhookAuth, {
     credentials: {
-      password: {
+      token: {
         type: "password",
         required: true,
       },
