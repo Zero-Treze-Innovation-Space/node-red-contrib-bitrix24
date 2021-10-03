d9db8cdf1674b051d502b3cb6ead371942049a4b
Diego Lopes <diego-chez@hotmail.com>
2021-10-03 13:26:32 -0300

    feat: list leads registered

--- nodes/bitrix-lead.js
+++ nodes/bitrix-lead.js
@@ -1,5 +1,9 @@
+const createClient = require("../lib/client")
+const createLeadService = require("../lib/services/lead-service");
+const createStatusNotifier = require("../lib/status-notifier");
+
 module.exports = function (RED) {
-  "use strict";  
+  "use strict";
   
   function BitrixLead(n) {
     RED.nodes.createNode(this, n)
@@ -8,11 +12,25 @@ module.exports = function (RED) {
     const authNode = RED.nodes.getNode(n.auth)
     const portalName = authNode.portalName
     const token = authNode.credentials.token
+    const method = n.method
     
+    const client = createClient({ portalName, token })
+    const leadService = createLeadService({ client })
+
     this.on("input", ((msg, send, done) => {
-      msg.payload = { portalName, token }
-      send(msg)
-      done()
+      this.status({})
+      const { payload, query } = msg
+      leadService[method]({ payload, query })
+        .then(data => {
+          msg.payload = data
+          send(msg)
+        })
+        .catch(({ response }) => {
+          const { status, statusText } = response
+          const text = `[${status}] ${statusText}`
+          this.status({ text, fill: "red", shape: "ring" })
+        })
+        .finally(() => done())
     }))
   }
   RED.nodes.registerType("bitrix-lead", BitrixLead);
